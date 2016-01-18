57965f88d8c122d1974d3b062c854bdd3a61ccad
abitmore <abitmore@users.noreply.github.com>
2016-01-19 07:09:37 +0800

    Register transfer_v2_evaluator

--- libraries/chain/db_init.cpp
+++ libraries/chain/db_init.cpp
@@ -171,6 +171,7 @@ void database::initialize_evaluators()
    register_evaluator<transfer_from_blind_evaluator>();
    register_evaluator<blind_transfer_evaluator>();
    register_evaluator<asset_claim_fees_evaluator>();
+   register_evaluator<transfer_v2_evaluator>();
 }
 
 void database::initialize_indexes()
