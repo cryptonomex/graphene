5da0bf0e94f19de01186b671bc528433302b42c1
abitmore <abitmore@users.noreply.github.com>
2016-02-11 05:50:02 +0800

    Real fix for transfer_v2_operation::calculate_fee(,CORE_ASSET,) issue

--- libraries/chain/protocol/transfer.cpp
+++ libraries/chain/protocol/transfer.cpp
@@ -68,7 +68,8 @@ share_type transfer_v2_operation::calculate_fee( const fee_parameters_type& sche
 {
    share_type core_fee_required;
    auto o = asset_obj.get_transfer_fee_mode();
-   if( o == asset_transfer_fee_mode_flat || asset_obj.options.core_exchange_rate.is_null() ) // flat fee mode
+   if( o == asset_transfer_fee_mode_flat
+         || ( asset_obj.options.core_exchange_rate.is_null() && asset_obj.id != asset_id_type() )  ) // flat fee mode
    {
       auto core_fee_128 = fc::uint128(schedule.flat_fee);
       core_fee_128 *= scale;
