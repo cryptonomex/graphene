a2de0769209033ce135d44d6d339986d1c1423c4
abitmore <abitmore@users.noreply.github.com>
2016-02-15 03:37:33 +0800

    Update coin_seconds_earned and etc when creating new balance object

--- libraries/chain/db_balance.cpp
+++ libraries/chain/db_balance.cpp
@@ -68,6 +68,17 @@ void database::adjust_balance(account_id_type account, asset delta )
          b.asset_type = delta.asset_id;
          b.balance = delta.amount.value;
       });
+      // Update coin_seconds_earned and etc
+      if( head_block_time() > HARDFORK_FREE_TRX_TIME )
+      {
+         // TODO how to deal with smart coins?
+         if( delta.asset_id == asset_id_type() )
+         {
+            modify( account(*this).statistics(*this), [&](account_statistics_object& s) {
+               s.set_coin_seconds_earned( 0, head_block_time() );
+            });
+         }
+      }
    } else {
       if( delta.amount < 0 )
          FC_ASSERT( itr->get_balance() >= -delta, "Insufficient Balance: ${a}'s balance of ${b} is less than required ${r}", ("a",account(*this).name)("b",to_pretty_string(itr->get_balance()))("r",to_pretty_string(-delta)));
