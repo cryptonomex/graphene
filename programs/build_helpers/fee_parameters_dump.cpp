c133dff431403417db711b372bacd12204ee891f
theoreticalbts <theoreticalbts@users.noreply.github.com>
2016-03-11 15:03:55 -0500

    fixup missing semicolon

--- programs/build_helpers/fee_parameters_dump.cpp
+++ programs/build_helpers/fee_parameters_dump.cpp
@@ -104,7 +104,7 @@ struct dump_fee_struct_visitor
 
       ctx->out << "template<>\n"
          "struct fee_parameters_type< " << optype << " >\n"
-         "{\n"
+         "{\n";
       fc::reflector<Op::fee_parameters_type>::visit( vtor );
       ctx->out << "};\n\n";
 
